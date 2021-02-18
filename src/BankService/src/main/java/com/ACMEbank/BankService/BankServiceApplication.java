f892d7ac72b7db50317c5919c3317c7a93f04841
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2021-02-18 15:29:26 +0100

    Add http filters

--- src/BankService/src/main/java/com/ACMEbank/BankService/BankServiceApplication.java
+++ src/BankService/src/main/java/com/ACMEbank/BankService/BankServiceApplication.java
@@ -1,9 +1,16 @@
 package com.ACMEbank.BankService;
 
+import com.ACMEbank.BankService.security.JWTAuthorizationFilter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -14,8 +21,30 @@ public class BankServiceApplication {
 		SpringApplication.run(BankServiceApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+	@EnableWebSecurity
+	@Configuration
+	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable()
+				.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+				.authorizeRequests()
+				.antMatchers(HttpMethod.POST, "/users").permitAll()
+				.antMatchers(HttpMethod.POST, "/auth").permitAll()
+				.antMatchers(HttpMethod.GET, "/payments/*").permitAll()
+				.antMatchers(HttpMethod.PUT, "/payments/*").permitAll()
+				.antMatchers("/").permitAll()
+				.antMatchers("/link/**").permitAll()
+				.antMatchers("/h2/**").permitAll()
+				.antMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
+				.anyRequest().authenticated();
+			http.headers().frameOptions().disable();
+		}
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "Welcome ACMEbank";
 	}
 }
