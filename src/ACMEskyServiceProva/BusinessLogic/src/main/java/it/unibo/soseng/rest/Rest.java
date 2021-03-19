fa398d576ad5673d4872fbaacb96b0ca90a99885
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2021-03-19 10:38:46 +0100

    Update

---
+++ src/ACMEskyServiceProva/BusinessLogic/src/main/java/it/unibo/soseng/rest/Rest.java
@@ -0,0 +1,9 @@
+package it.unibo.soseng.rest;
+
+public class Rest {
+    public int n; 
+  
+    public Rest(int n){ 
+        this.n = n;
+    } 
+}
---
+++ src/ACMEskyServiceProva/Gateway/src/main/java/it/unibo/soseng/Prova.java
@@ -0,0 +1,11 @@
+package it.unibo.soseng;
+
+import it.unibo.soseng.rest.Rest;
+
+public class Prova {
+    final Rest rest;
+
+    public Prova(){
+        rest = new Rest(99);
+    }
+}
