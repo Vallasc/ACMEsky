b6a57876cc09b7b425d1a52186c1312950300cc9
andiub97 <43032657+andiub97@users.noreply.github.com>
2021-03-26 17:35:41 +0100

    Delete

--- src/ACMEskyService/src/main/java/it/unibo/soseng/logic/airline/AirlineManager.java
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/logic/airline/AirlineManager.java
@@ -1,13 +1,7 @@
 package it.unibo.soseng.logic.airline;
 
-import java.util.List;
 
-import it.unibo.soseng.model.UserRequest;
 
 public class AirlineManager {
     
-    public List<UserRequest> retrieveRequestFromDB(){
-        return null;
-        
-    }
 }
