3d9282ab31f3976dc08637c0f03ef4460a9efc5d
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2021-03-28 23:59:52 +0200

    Add dto

---
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/gateway/airline/dto/AirlineFlightOffer.java
@@ -0,0 +1,72 @@
+package it.unibo.soseng.gateway.airline.dto;
+
+import java.io.Serializable;
+
+public class AirlineFlightOffer implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+
+    private String flightCode;
+    private String departureAirportCode;
+    private String arrivalAirportCode;
+    private long departureTimestamp;
+    private long arrivalTimestamp;
+    private long expireTimestamp;
+    private float price;
+
+    public String getFlightCode() {
+        return flightCode;
+    }
+
+    public void setFlightCode(String flightCode) {
+        this.flightCode = flightCode;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public long getExpireTimestamp() {
+        return expireTimestamp;
+    }
+
+    public void setExpireTimestamp(long expireTimestamp) {
+        this.expireTimestamp = expireTimestamp;
+    }
+
+    public long getArrivalTimestamp() {
+        return arrivalTimestamp;
+    }
+
+    public void setArrivalTimestamp(long arrivalTimestamp) {
+        this.arrivalTimestamp = arrivalTimestamp;
+    }
+
+    public long getDepartureTimestamp() {
+        return departureTimestamp;
+    }
+
+    public void setDepartureTimestamp(long departureTimestamp) {
+        this.departureTimestamp = departureTimestamp;
+    }
+
+    public String getArrivalAirportCode() {
+        return arrivalAirportCode;
+    }
+
+    public void setArrivalAirportCode(String arrivalAirportCode) {
+        this.arrivalAirportCode = arrivalAirportCode;
+    }
+
+    public String getDepartureAirportCode() {
+        return departureAirportCode;
+    }
+
+    public void setDepartureAirportCode(String departureAirportCode) {
+        this.departureAirportCode = departureAirportCode;
+    }
+}
