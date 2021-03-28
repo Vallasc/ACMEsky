336ee6845572bb763e1b148ade8027f9fb1defb9
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2021-03-28 23:59:15 +0200

    Add role

--- src/ACMEskyService/src/main/java/it/unibo/soseng/gateway/JaxRsActivator.java
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/gateway/JaxRsActivator.java
@@ -1,9 +1,14 @@
 package it.unibo.soseng.gateway;
 
+import static it.unibo.soseng.security.Constants.BANK;
+import static it.unibo.soseng.security.Constants.AIRLINE;
+
+import javax.annotation.security.DeclareRoles;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+@DeclareRoles({BANK, AIRLINE})
 @ApplicationPath("/")
 public class JaxRsActivator extends Application {
-
+    
 }
