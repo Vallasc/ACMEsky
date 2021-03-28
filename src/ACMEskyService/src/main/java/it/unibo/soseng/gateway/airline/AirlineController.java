40630738c0fd055092ea9096aa36a9b47291294d
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2021-03-28 23:59:35 +0200

    Add last-minute route

--- src/ACMEskyService/src/main/java/it/unibo/soseng/gateway/airline/AirlineController.java
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/gateway/airline/AirlineController.java
@@ -6,39 +6,34 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.inject.Inject;
+import javax.security.enterprise.SecurityContext;
 
-import it.unibo.soseng.gateway.airline.dao.OfferRequest;
+import it.unibo.soseng.gateway.airline.dto.AirlineFlightOffer;
 import it.unibo.soseng.logic.airline.AirlineManager;
 
-@Path("airline")
+@Path("airlines")
 public class AirlineController {
     private final static Logger LOGGER = Logger.getLogger(AirlineController.class.getName());
-
+    
     @Inject
     AirlineManager airlineManager;
 
+    @Inject
+    SecurityContext securityContext;
+
     @POST
-    @Path("/offers")
+    @Path("/last_minute")
     @Consumes( MediaType.APPLICATION_JSON )
-    public Response saveOffer(OfferRequest offer, @Context UriInfo uriInfo) {
-        LOGGER.info("POST offers");
-        //airlineManager.saveAirlineOffer(offer.toFlight());
-        return Response.status(Response.Status.CREATED.getStatusCode())
-                        .header("Location", String.format("%s/%s", uriInfo.getAbsolutePath().toString(), offer.getFlightId()))
-                        .build();
+    public Response saveLastMinute(List<AirlineFlightOffer> offers) {
+        String name = securityContext.getCallerPrincipal().getName();
+        airlineManager.startSaveLastMinuteProcess(offers, name);
+        return Response.status(Response.Status.CREATED.getStatusCode()).build();
     }
 
-    @GET
-    public Response createAirport() {
-        LOGGER.info("GET airport");
-        //airlineManager.saveAirlineOffer(offer.toFlight());
-        airlineManager.createAirport();
-        return Response.status(Response.Status.OK.getStatusCode()).build();
-    }
 }
