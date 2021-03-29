0e4cddfbd3a753a03bb24b314a1b47aeab4fe63d
andiub97 <43032657+andiub97@users.noreply.github.com>
2021-03-29 17:56:04 +0200

    Add retrieve flight

--- src/ACMEskyService/src/main/java/it/unibo/soseng/logic/database/DatabaseManager.java
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/logic/database/DatabaseManager.java
@@ -1,5 +1,41 @@
 package it.unibo.soseng.logic.database;
 
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+
+import it.unibo.soseng.model.FlightInterest;
+
+
 public class DatabaseManager {
     
+    private final static Logger LOGGER = Logger.getLogger(DatabaseManager.class.getName());
+
+    
+
+    public static EntityManagerFactory emf = Persistence.createEntityManagerFactory("java:jboss/datasources/postgresacmesky");
+
+    public static EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public List<FlightInterest> retrieveFlightInterests(){
+
+        EntityManager em = getEntityManager();
+        List<FlightInterest> interests = 
+                                em.createQuery("SELECT flight.departure_airport_id, flight.departure arrival_airport_id,"+
+                                                            "flight.departure_date_time, flight.arrival_date_time " +
+                                                            "FROM flights_interest flight")
+                                            .getResultList();
+            return interests;
+    }
 }
