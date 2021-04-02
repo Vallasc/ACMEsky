b52441db644ca02485d87f074418b832503439d8
Vallasc <giacomovallorani.97@gmail.com>
2021-04-02 10:39:21 +0200

    Change file name

---
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/model/GeneratedOffer.java
@@ -0,0 +1,92 @@
+package it.unibo.soseng.model;
+
+import java.io.Serializable;
+import java.time.ZonedDateTime;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "generated_offers")
+public class GeneratedOffer implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "outbound_flight_id")
+    private Flight outboundFlight;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "flight_back_id")
+    private Flight flightBack;
+
+    @Column(name = "expire_date", nullable = false)
+    private ZonedDateTime expireDate;
+
+    @Column(name = "total_price", nullable = true)
+    private double totalPrice;
+
+    @Column(name = "booked", nullable = true)
+    private boolean booked;
+
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Flight getOutboundFlightId() {
+        return this.outboundFlight;
+    }
+
+    public void setOutboundFlightId(Flight outboundFlight) {
+        this.outboundFlight = outboundFlight;
+    }
+
+    public Flight getFlightBackId() {
+        return this.flightBack;
+    }
+
+    public void setFlightBackId(Flight flightBack) {
+        this.flightBack = flightBack;
+    }
+
+    public ZonedDateTime getExpireDate() {
+        return this.expireDate;
+    }
+
+    public void setExpireDate(ZonedDateTime expireDate) {
+        this.expireDate = expireDate;
+    }
+
+    public double getTotalPrice() {
+        return this.totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public boolean getBooked() {
+        return this.booked;
+    }
+
+    public void setBooked(boolean booked) {
+        this.booked = booked;
+    }
+}
---
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/model/UserInterest.java
@@ -0,0 +1,83 @@
+package it.unibo.soseng.model;
+
+import java.io.Serializable;
+import java.time.ZonedDateTime;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users_interests")
+public class UserInterest implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private long id;
+
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "user_id")
+    private User user;
+
+	@OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "outbound_flight_interest_id")
+	private FlightInterest outboundFlightInterest;
+
+	@OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "flight_back_interest_id")
+    private FlightInterest flightBackInterest;
+
+    @Column(name = "expire_date", nullable = true)
+	private ZonedDateTime expireDate;
+	
+
+	public long getId() {
+		return this.id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return this.user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public FlightInterest getOutboundFlightInterest() {
+		return this.outboundFlightInterest;
+	}
+
+	public void setOutboundFlightInterest(FlightInterest outboundFlightInterest) {
+		this.outboundFlightInterest = outboundFlightInterest;
+	}
+    
+	public FlightInterest getFlightBackInterest() {
+		return this.flightBackInterest;
+	}
+
+	public void setFlightBackInterest(FlightInterest flightBackInterest) {
+		this.flightBackInterest = flightBackInterest;
+	}
+    
+	public ZonedDateTime getExpireDate() {
+		return this.expireDate;
+	}
+	
+	public void setgetExpireDate(ZonedDateTime expireDate) {
+		this.expireDate = expireDate;
+	}
+}
