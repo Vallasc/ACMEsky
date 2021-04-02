767f6f20890641134415560145f258bb5a0410fe
Vallasc <giacomovallorani.97@gmail.com>
2021-04-02 10:36:53 +0200

    Add user interest

--- src/ACMEskyService/src/main/java/it/unibo/soseng/logic/airline/AirlineManager.java
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/logic/airline/AirlineManager.java
@@ -1,5 +1,8 @@
 package it.unibo.soseng.logic.airline;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
@@ -9,31 +12,42 @@ import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionManagement;
 import javax.ejb.TransactionManagementType;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.security.enterprise.SecurityContext;
 
 import org.camunda.bpm.engine.ProcessEngines;
 import org.camunda.bpm.engine.RuntimeService;
 
 import it.unibo.soseng.gateway.airline.dto.AirlineFlightOffer;
+import it.unibo.soseng.gateway.user.dto.InterestsRequest;
+import it.unibo.soseng.logic.database.DatabaseManager;
+import it.unibo.soseng.logic.database.DatabaseManager.AirportNotFoundException;
+import it.unibo.soseng.logic.database.DatabaseManager.UserNotFoundException;
 import it.unibo.soseng.model.Airport;
+import it.unibo.soseng.model.FlightInterest;
+import it.unibo.soseng.model.User;
+import it.unibo.soseng.model.UserInterest;
+
 import static it.unibo.soseng.camunda.StartEvents.SAVE_LAST_MINUTE;
+import static it.unibo.soseng.camunda.StartEvents.SAVE_INTERESTS;
 import static it.unibo.soseng.camunda.ProcessVariables.AIRLINE_FLIGHT_OFFERS;
 import static it.unibo.soseng.camunda.ProcessVariables.AIRLINE_NAME;
+import static it.unibo.soseng.camunda.ProcessVariables.USER_INTERESTS_REQUEST;
+import static it.unibo.soseng.camunda.ProcessVariables.USERNAME;
 
 @Stateless
-@TransactionManagement(value = TransactionManagementType.CONTAINER)
 public class AirlineManager {
     private final static Logger LOGGER = Logger.getLogger(AirlineManager.class.getName());
+
+    @Inject
+    private DatabaseManager databaseManager;
     
-    @PersistenceContext(unitName = "primary")
-    private EntityManager entityManager;
+    @Inject
+    private SecurityContext securityContext;
 
-
-    /**
-     * Invia il messaggio di start per il processo SaveOffer
-     * @param airlineLastMinuteOffers
-     */
+    // Camunda
     public void startSaveLastMinuteProcess(List<AirlineFlightOffer> airlineLastMinuteOffers, String airlineName){
         LOGGER.info("StartSaveLastMinuteProcess");
         RuntimeService runtimeService = ProcessEngines.getDefaultProcessEngine().getRuntimeService();
@@ -43,17 +57,76 @@ public class AirlineManager {
         runtimeService.startProcessInstanceByMessage(SAVE_LAST_MINUTE, processVariables);
     }
 
+    public void startSaveUserInterests(InterestsRequest userInterestsRequest, String username) 
+                                                                throws UserNotAllowedException{
+        LOGGER.info("StartSaveUserInterests");
+
+        String loginName = securityContext.getCallerPrincipal().getName();
+        if(username.equals(loginName))
+            throw new UserNotAllowedException();
+
+        RuntimeService runtimeService = ProcessEngines.getDefaultProcessEngine().getRuntimeService();
+        Map<String,Object> processVariables = new HashMap<String,Object>();
+        processVariables.put(USER_INTERESTS_REQUEST, userInterestsRequest);
+        processVariables.put(USERNAME, username);
+        runtimeService.startProcessInstanceByMessage(SAVE_INTERESTS, processVariables);
+    }
+
     public void saveAirlineOffers(List<AirlineFlightOffer> airlineLastMinuteOffers, String airlineName){
         LOGGER.log(Level.INFO, "Save offer of airline {0}", airlineName);
     }
 
-    public void createAirport() {
-        Airport airport = new Airport();
-        airport.setAiportCode("das");
-        airport.setName("asddas");
-        airport.setAddress("asaaaaa");
-        this.entityManager.persist(airport);
-        this.entityManager.flush();
-        LOGGER.info(airport.getId() + "");
+    public void saveUserInterests(InterestsRequest request, String username) 
+                            throws AirportNotFoundException, UserNotFoundException {
+
+        // Check airport
+        Airport airportOut1 = databaseManager.getAirport(request.getOutboundFlight().getArrivalAirportCode());
+        Airport airportOut2 = databaseManager.getAirport(request.getOutboundFlight().getDepartureAirportCode());
+        Airport airportBack1 = null;
+        Airport airportBack2 = null;
+        if(request.getFlightBack() != null){
+            airportBack1 = databaseManager.getAirport(request.getFlightBack().getArrivalAirportCode());
+            airportBack2 = databaseManager.getAirport(request.getFlightBack().getDepartureAirportCode());
+        }
+
+        // Get User
+        User user = databaseManager.getUser(username);
+
+        UserInterest interest = new UserInterest();
+
+        FlightInterest flightOutInterest = new FlightInterest();
+        flightOutInterest.setUser(user);
+        flightOutInterest.setDepartureAirport(airportOut1);
+        flightOutInterest.setArrivalAirport(airportOut2);
+
+        flightOutInterest.setDepartureDateTime(
+            ZonedDateTime.ofInstant(Instant.ofEpochMilli(request.getOutboundFlight().getDepartureTimestamp()), 
+            ZoneId.systemDefault()));
+        flightOutInterest.setArrivalDateTime(
+            ZonedDateTime.ofInstant(Instant.ofEpochMilli(request.getOutboundFlight().getArrivalTimestamp()), 
+            ZoneId.systemDefault()));
+        
+        interest.setFlightBackInterest(flightOutInterest);
+
+        if(request.getFlightBack() != null){
+            FlightInterest flightBackInterest = new FlightInterest();
+            flightBackInterest.setUser(user);
+            flightBackInterest.setDepartureAirport(airportBack1);
+            flightBackInterest.setArrivalAirport(airportBack2);
+    
+            flightBackInterest.setDepartureDateTime(
+                ZonedDateTime.ofInstant(Instant.ofEpochMilli(request.getFlightBack().getDepartureTimestamp()), 
+                ZoneId.systemDefault()));
+                flightBackInterest.setArrivalDateTime(
+                ZonedDateTime.ofInstant(Instant.ofEpochMilli(request.getFlightBack().getArrivalTimestamp()), 
+                ZoneId.systemDefault()));
+
+            interest.setFlightBackInterest(flightBackInterest);
+        }
+
+        databaseManager.saveUserInterest(interest);
     }
+
+    public class UserNotAllowedException extends Exception {
+        private static final long serialVersionUID = 1L;}
 }
