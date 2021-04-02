fc60d00e52138f1f0f6dcf5fa25e7e1185d6038b
Vallasc <giacomovallorani.97@gmail.com>
2021-04-02 14:21:34 +0200

    Change id to uuid

--- src/ACMEskyService/src/main/java/it/unibo/soseng/model/UserInterest.java
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/model/UserInterest.java
@@ -2,6 +2,7 @@ package it.unibo.soseng.model;
 
 import java.io.Serializable;
 import java.time.ZonedDateTime;
+import java.util.UUID;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -14,6 +15,8 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.GenericGenerator;
+
 @Entity
 @Table(name="users_interests")
 public class UserInterest implements Serializable {
@@ -21,9 +24,10 @@ public class UserInterest implements Serializable {
     private static final long serialVersionUID = 1L;
 
 	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(generator = "UUID")
+	@GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     @Column(name = "id", nullable = false)
-    private long id;
+    private UUID id;
 
 	@ManyToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "user_id")
@@ -41,11 +45,11 @@ public class UserInterest implements Serializable {
 	private ZonedDateTime expireDate;
 	
 
-	public long getId() {
+	public UUID getId() {
 		return this.id;
 	}
 
-	public void setId(long id) {
+	public void setId(UUID id) {
 		this.id = id;
 	}
 
