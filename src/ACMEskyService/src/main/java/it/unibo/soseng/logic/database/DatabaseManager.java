32d149821024f8e3c6c30e15f6eb664af0645288
Vallasc <giacomovallorani.97@gmail.com>
2021-04-02 10:37:21 +0200

    Add user query

--- src/ACMEskyService/src/main/java/it/unibo/soseng/logic/database/DatabaseManager.java
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/logic/database/DatabaseManager.java
@@ -9,51 +9,74 @@ import javax.ejb.TransactionManagementType;
 import javax.enterprise.context.RequestScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
 
+import it.unibo.soseng.model.Airport;
 import it.unibo.soseng.model.FlightInterest;
 import it.unibo.soseng.model.User;
+import it.unibo.soseng.model.UserInterest;
 
 @RequestScoped
 @TransactionManagement(value = TransactionManagementType.CONTAINER)
 public class DatabaseManager {
-    
+
     private final static Logger LOGGER = Logger.getLogger(DatabaseManager.class.getName());
 
-    
     @PersistenceContext(unitName = "primary")
     private EntityManager entityManager;
 
-
-    public List<FlightInterest> retrieveFlightInterests(){
+    public List<FlightInterest> retrieveFlightInterests() {
         @SuppressWarnings("unchecked")
-        List<FlightInterest> interests = 
-        entityManager.createQuery("SELECT flight.departure_airport_id, flight.departure arrival_airport_id,"+
-                                                            "flight.departure_date_time, flight.arrival_date_time " +
-                                                            "FROM flights_interest flight")
-                                            .getResultList();
+        List<FlightInterest> interests = entityManager
+                .createQuery("SELECT flight.departure_airport_id, flight.departure arrival_airport_id,"
+                        + "flight.departure_date_time, flight.arrival_date_time " + "FROM flights_interest flight")
+                .getResultList();
         return interests;
     }
 
-
-    public User getUser(String username) throws UserNotFoundException{
+    public User getUser(String username) throws UserNotFoundException {
         @SuppressWarnings("unchecked")
-        List<User> result = (List<User>) entityManager.createQuery(
-                            "SELECT u FROM User u, DomainEntity e WHERE e.username = :username AND u.entity = e.id")
-                            .setParameter("username", username)
-                            .setMaxResults(10)
-                            .getResultList();
-        if( result.size() == 1 ){
+        List<User> result = (List<User>) entityManager
+                .createQuery("SELECT u FROM User u, DomainEntity e WHERE e.username = :username AND u.entity = e.id")
+                .setParameter("username", username).getResultList();
+        if (result.size() == 1) {
             return result.get(0);
         }
         throw new UserNotFoundException();
     }
 
-    public void createUser(User user){
-        //find
-        this.entityManager.persist(user);
+    public void createUser(User user) throws UserAlreadyInException {
+        try {
+            this.entityManager.persist(user);
+        } catch (PersistenceException e) {
+            throw new UserAlreadyInException();
+        }
     }
 
-    public class UserNotFoundException extends Exception{
+    public Airport getAirport(String code) throws AirportNotFoundException {
+        @SuppressWarnings("unchecked")
+        List<Airport> result = (List<Airport>) entityManager
+                .createQuery("SELECT a FROM Airpot a WHERE a.code = :code")
+                .setParameter("code", code).getResultList();
+        if (result.size() == 1) {
+            return result.get(0);
+        }
+        throw new AirportNotFoundException();
+    }
+
+    public void saveUserInterest(UserInterest interest) {
+        this.entityManager.persist(interest);
+    }
+
+    public class UserNotFoundException extends Exception {
+        private static final long serialVersionUID = 1L;
+    }
+
+    public class UserAlreadyInException extends Exception {
+        private static final long serialVersionUID = 1L;
+    }
+
+    public class AirportNotFoundException extends Exception {
         private static final long serialVersionUID = 1L;
     }
 }
