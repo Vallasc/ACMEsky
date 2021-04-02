d7c5f5421fd55ec1dd1612671b27711e86ba4577
Vallasc <giacomovallorani.97@gmail.com>
2021-04-02 10:33:11 +0200

    Add save interest

--- src/ACMEskyService/src/main/java/it/unibo/soseng/camunda/interest_manager/SaveInterstsDelegate.java
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/camunda/interest_manager/SaveInterstsDelegate.java
@@ -3,18 +3,34 @@ package it.unibo.soseng.camunda.interest_manager;
 import java.util.logging.Logger;
 
 import javax.inject.Named;
+import javax.inject.Inject;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+import it.unibo.soseng.logic.airline.AirlineManager;
+import it.unibo.soseng.gateway.user.dto.InterestsRequest;
+import static it.unibo.soseng.camunda.ProcessVariables.USER_INTERESTS_REQUEST;
+import static it.unibo.soseng.camunda.ProcessVariables.USERNAME;
 
 @Named("saveInterstsDelegate")
 public class SaveInterstsDelegate implements JavaDelegate {
 
     private final static Logger LOGGER = Logger.getLogger(SaveInterstsDelegate.class.getName());
 
+    @Inject 
+    AirlineManager airlineManager;
+
     @Override
     public void execute(DelegateExecution execution){
-      LOGGER.info("Execute");
+        LOGGER.info("Execute");
+        InterestsRequest interest = (InterestsRequest) execution.getVariable(USER_INTERESTS_REQUEST);
+        String username = (String) execution.getVariable(USERNAME);
+        try {
+            airlineManager.saveUserInterests(interest, username);
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
     }
   
 }
