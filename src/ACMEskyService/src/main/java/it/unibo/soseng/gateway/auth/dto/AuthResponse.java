26efc62398e9f755a19d2876d5f94dd7e9a7c3b8
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2021-06-22 11:46:47 +0200

    Initial commit

---
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/gateway/auth/dto/AuthResponse.java
@@ -0,0 +1,13 @@
+package it.unibo.soseng.gateway.auth.dto;
+
+public class AuthResponse {
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
