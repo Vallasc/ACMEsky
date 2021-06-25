dd8b81f8ff74ba1b4618e83895d75e14f096fb0f
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2021-06-25 16:36:39 +0200

    Remove name, surname, address

--- src/ACMEskyService/src/main/java/it/unibo/soseng/gateway/user/dto/UserResponse.java
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/gateway/user/dto/UserResponse.java
@@ -1,36 +1,9 @@
 package it.unibo.soseng.gateway.user.dto;
 
 public class UserResponse {
-    private String name;
-    private String surname;
     private String email;
     private String password;
     private String prontogramToken;
-    private String address;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
 
     public String getProntogramToken() {
         return prontogramToken;
