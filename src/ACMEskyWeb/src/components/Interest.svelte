054aea404a4992f724abd23e330670c516b237f7
Vallasc <giacomovallorani.97@gmail.com>
2021-07-02 20:20:44 +0200

    Add submit and fetch

--- src/ACMEskyWeb/src/components/Interest.svelte
+++ src/ACMEskyWeb/src/components/Interest.svelte
@@ -1,93 +1,161 @@
 <script lang="ts">
-    import { fade, fly } from "svelte/transition";
+    import { fade, fly } from "svelte/transition"
+    import { Input } from 'sveltestrap'
+    import { fetchAirports, fetchAirport, createInterest } from "../logic"
+    import type { Airport, Interest } from "../types"
+
+    const INTEREST_WINDOW : number = 365
 
     let today : number = Date.now()
     let day : number = 1000 * 60 * 60 * 24
-    let startDate : Date = new Date(today + day * 2)
-    let endDate : Date=  new Date(today + day * 120)
+    let startDate : Date = new Date(today + day * 1)
+    let endDate : Date =  new Date(today + day * INTEREST_WINDOW)
 
     function formatDate (date : Date) : string {
         return date.toISOString().split("T")[0]
     }
 
-    let departure : string 
-    let arrival : string 
-    let ar : boolean
-    let airports : Array<string> = ["Roma", "Milano"]
+    let departureCode : string 
+    let arrivalCode : string 
+    let ar : boolean = false
+    let dateA : string = Date()
+    let dateR : string
 
-    function validate() : boolean {
-        if(departure == arrival)
-            return true
-        return false
+    let invalidDeparture : boolean = false
+    let invalidArrival : boolean  = false
+    let invalidDateA : boolean = false
+    let invalidDateR : boolean = false
+
+    let airports : Array<Airport> = []
+    let form : HTMLFormElement
+
+    async function validate() : Promise<boolean> {
+        if(departureCode === arrivalCode){
+            invalidArrival = true
+            return false
+        }
+        if(ar && (new Date(dateA)).getTime() >= (new Date(dateR)).getTime()){
+            invalidDateR = true
+            return false
+        }
+
+        let airport : Airport | null = await fetchAirport(departureCode)
+        if(airport == null || airport.code !== departureCode){
+            invalidDeparture = true
+            return false
+        }
+
+        if( ar ){
+            airport = await fetchAirport(arrivalCode)
+            if(airport == null || airport.code !== arrivalCode){
+                invalidArrival = true
+                return false
+            }
+        }
+        return true
     }
 
-    let timer;
-    function fetchAirport(target : HTMLInputElement) : void {
+    function resetForm() : void {
+        form.reset()
+        invalidDeparture = false
+        invalidArrival = false
+        invalidDateA = false
+        invalidDateR = false
+        ar = false
+    }
+
+
+    let timer : NodeJS.Timeout;
+    function fetch(target : any) : void {
         let value : string = target.value
         clearTimeout(timer);
-        timer = setTimeout(() => {
+        timer = setTimeout(async () => {
             //val = v;
             console.log(value)
-        }, 300);
+            airports = await fetchAirports(value)
+            console.log(airports)
+        }, 300)
     }
 
-    function submit() {}
+    async function submit() {
+        if( ! (await validate()) ) return
+
+        let request : Interest = {
+            outboundFlight : {
+                departureAirportCode : departureCode,
+                arrivalAirportCode : arrivalCode,
+                departureTimestamp : (new Date(dateA)).toISOString()
+            },
+            flightBack : !ar ? null : {
+                departureAirportCode : arrivalCode,
+                arrivalAirportCode : departureCode,
+                departureTimestamp : (new Date(dateR)).toISOString()
+            }
+        }
+        createInterest(request)
+        resetForm()
+    }
 </script>
 
 <div class="main">
-    <h1 class="h1 mb-4 mt-4 fw-normal">Aggiungi i tuoi interessi</h1>
-    <div class="card mt-4">
-        <form on:submit|preventDefault={submit}>
+    <h1 class="h1 mt-4 fw-normal">Aggiungi voli di interesse</h1>
+    <div class="card mt-4 mb-4" in:fade = {{duration: 200}}>
+        <form on:submit|preventDefault={submit} bind:this={form}>
             <div class="row g-3">
                 <div class="col-md-12">
                     <h1 class="h4 fw-normal">Andata</h1>
                 </div>
-                <div class="col-md-4">
+                <datalist id="datalistOptions">
+                    {#each airports as airport}
+                        <option value={airport.code}>{airport.name} ({airport.code}) - {airport.cityName}</option>
+                    {/each}
+                </datalist>
+                <div class="col-md-6">
                     <label for="_" class="form-label">Partenza</label>
-                    <input bind:value={departure} on:keyup={({ target }) => fetchAirport(target)} class="form-control" list="datalistOptions" placeholder="Areoporto di partenza">
-                    <datalist id="datalistOptions">
-                        {#each airports as airport}
-                            <option value={airport}>
-                        {/each}
-                    </datalist>
+                    <Input bind:value={departureCode} on:keyup={({target}) => fetch(target)} on:focus={() => airports = []}
+                            invalid = {invalidDeparture} feedback="Seleziona il codice dalla lista"
+                            class="form-control" list="datalistOptions" placeholder="Areoporto di partenza" required />
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-6">
                     <label for="_" class="form-label">Arrivo</label>
-                    <input bind:value={arrival} class="form-control" list="datalistOptions" placeholder="Areoporto di arrivo">
-                    <datalist id="datalistOptions">
-                        <option value="Roma">
-                        <option value="Parigi">
-                        <option value="Berlino">
-                    </datalist>
+                    <Input bind:value={arrivalCode} on:keyup={({target}) => fetch(target)} on:focus={() => airports = []}
+                        invalid = {invalidArrival} feedback="Seleziona il codice dalla lista"
+                        class="form-control" list="datalistOptions" placeholder="Areoporto di partenza" required />
                 </div>
 
-                <div class="col-4">
+                <div class="col-md-6">
                     <label for="_" class="form-label">Data</label>
-                    <input type="date" class="form-control" min={formatDate(startDate)} max={formatDate(endDate)}>
+                    <Input bind:value={dateA} type="date" class="form-control" 
+                        min={formatDate(startDate)} max={formatDate(endDate)} 
+                        invalid = {invalidDateA} feedback="Seleziona il codice dalla lista" required />
                 </div>
-                <div class="checkbox mb-3">
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" bind:checked={ar}/>
-                        <label class="form-check-label" for="gridCheck">
-                            Andata e ritorno
-                        </label>
+                <div class="col-md-6 check">
+                    <div class="checkbox">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" bind:checked={ar}/>
+                            <label class="form-check-label" for="gridCheck">
+                                Andata e ritorno
+                            </label>
+                        </div>
                     </div>
                 </div>
                 {#if ar}
                     <div class="col-md-12">
-                        <h1 class="h4 fw-normal">Ritorno</h1>
+                        <h1 class="h4 fw-normal mt-3">Ritorno</h1>
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-6">
                         <label for="_" class="form-label">Partenza</label>
-                        <input bind:value={arrival} class="form-control" disabled>
+                        <input bind:value={arrivalCode} class="form-control" disabled>
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-6">
                         <label for="_" class="form-label">Arrivo</label>
-                        <input bind:value={departure} class="form-control" disabled>
+                        <input bind:value={departureCode} class="form-control" disabled>
                     </div>
-                    <div class="col-4">
+                    <div class="col-6">
                         <label for="_" class="form-label">Data</label>
-                        <input type="date" class="form-control" min={formatDate(startDate)} max={formatDate(endDate)}>
+                        <Input bind:value={dateR} type="date" class="form-control" 
+                            min={formatDate(startDate)} max={formatDate(endDate)} 
+                            invalid = {invalidDateR} feedback="Seleziona il codice dalla lista" required />
                     </div>
                 {/if}
                 <div class="mt-4" />
@@ -102,7 +170,6 @@
 <style>
     .main {
         width: 100%;
-        height: 100%;
         display: flex;
         align-items: center;
         flex-direction: column;
@@ -123,6 +190,13 @@
     }
 
     .card {
-        max-width: 70%;
+        max-width: 60%;
+        margin-bottom: 40px;
+    }
+
+    .check {
+        display: flex;
+        align-items: flex-end;
+        justify-content: flex-start;
     }
 </style>
