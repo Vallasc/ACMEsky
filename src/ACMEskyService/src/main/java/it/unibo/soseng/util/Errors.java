678bea2ae834a811f897c18a1841e5823e12f940
Vallasc <giacomovallorani.97@gmail.com>
2021-07-02 20:29:30 +0200

    Initial commit

---
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/util/Errors.java
@@ -0,0 +1,11 @@
+package it.unibo.soseng.util;
+
+public class Errors {
+    public static final String DATE_FORMAT_ERROR = "Incorrect datetime format. Example 2007-04-05T14:30Z";
+    public static final String DATE_ERROR = "Date and time error";
+    public static final String DATE_TO_EARLY = "Date is too early";
+    public static final String DATE_TO_LATE = "Date is too late";
+    public static final String AIRPORT_NOT_FOUND = "Airport not found";
+    public static final String AIRPORT_CODE_ERROR = "Airports codes error";
+    public static final String USER_NOT_FOUND = "Username not found";
+}
