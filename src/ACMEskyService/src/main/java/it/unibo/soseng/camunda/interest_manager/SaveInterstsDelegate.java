3f94ff4bfe21f1d9729ccf2357cfeb3c9ce3aa39
Vallasc <giacomovallorani.97@gmail.com>
2021-07-02 20:33:46 +0200

    Code refactor

--- src/ACMEskyService/src/main/java/it/unibo/soseng/camunda/interest_manager/SaveInterstsDelegate.java
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/camunda/interest_manager/SaveInterstsDelegate.java
@@ -3,18 +3,19 @@ package it.unibo.soseng.camunda.interest_manager;
 import java.util.logging.Logger;
 
 import javax.inject.Named;
+import javax.ws.rs.core.Response;
 import javax.inject.Inject;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
-import it.unibo.soseng.logic.database.DatabaseManager.AirportNotFoundException;
-import it.unibo.soseng.logic.database.DatabaseManager.UserNotFoundException;
-import it.unibo.soseng.logic.airline.AirlineManager;
-import it.unibo.soseng.gateway.user.dto.InterestsRequest;
+import it.unibo.soseng.logic.interest.InterestManager;
+import it.unibo.soseng.camunda.ProcessState;
+import it.unibo.soseng.camunda.ProcessState.AsyncResponseDTO;
+import it.unibo.soseng.gateway.user.dto.UserInterestDTO;
+
 import static it.unibo.soseng.camunda.ProcessVariables.USER_INTERESTS_REQUEST;
 import static it.unibo.soseng.camunda.ProcessVariables.USERNAME;
-import static it.unibo.soseng.camunda.ProcessVariables.PROCESS_ERROR;
 
 @Named("saveInterstsDelegate")
 public class SaveInterstsDelegate implements JavaDelegate {
@@ -22,21 +23,19 @@ public class SaveInterstsDelegate implements JavaDelegate {
     private final static Logger LOGGER = Logger.getLogger(SaveInterstsDelegate.class.getName());
 
     @Inject 
-    AirlineManager airlineManager;
+    InterestManager interestManager;
+
+    @Inject
+    private ProcessState processState;
 
     @Override
     public void execute(DelegateExecution execution){
-        LOGGER.info("Execute");
-        InterestsRequest interest = (InterestsRequest) execution.getVariable(USER_INTERESTS_REQUEST);
-        String username = (String) execution.getVariable(USERNAME);
-        try {
-            airlineManager.saveUserInterests(interest, username);
-            execution.setVariable(PROCESS_ERROR, null);
-        } catch (AirportNotFoundException e) {
-            execution.setVariable(PROCESS_ERROR, "airport not found");
-        } catch (UserNotFoundException e) {
-            execution.setVariable(PROCESS_ERROR, "user not found");
-        }
+        LOGGER.info("Execute save interest");
+        UserInterestDTO interest = (UserInterestDTO) execution.getVariable(USER_INTERESTS_REQUEST);
+        String email = (String) execution.getVariable(USERNAME);
+        AsyncResponseDTO asyncResponse = processState.getResponse(email);
+        Response response = interestManager.handleUserInterests(asyncResponse, email, interest);
+        asyncResponse.getResponse().resume(response);
     }
   
 }
