8929ea83742a2deea29642e8c920622d9f429c39
Vallasc <giacomovallorani.97@gmail.com>
2021-07-03 13:10:11 +0200

    Remove saveUserInterests

--- src/ACMEskyService/src/main/java/it/unibo/soseng/logic/airline/AirlineManager.java
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/logic/airline/AirlineManager.java
@@ -31,7 +31,7 @@ import org.camunda.bpm.engine.runtime.ProcessInstanceWithVariables;
 import org.camunda.bpm.engine.task.Task;
 
 import java.net.http.HttpResponse;
-import it.unibo.soseng.gateway.airline.AirlineAPI;
+import it.unibo.soseng.gateway.airline.AirlineClient;
 import it.unibo.soseng.gateway.airline.dto.AirlineDTO;
 import it.unibo.soseng.gateway.airline.dto.AirlineFlightOffer;
 import it.unibo.soseng.gateway.airline.dto.InterestDTO;
@@ -63,7 +63,7 @@ public class AirlineManager {
     private SecurityContext securityContext;
 
     @Inject
-    AirlineAPI api;
+    AirlineClient api;
 
     // Camunda
     public void startSaveLastMinuteProcess(List<AirlineFlightOffer> airlineLastMinuteOffers, String airlineName){
@@ -79,58 +79,6 @@ public class AirlineManager {
     public void saveAirlineOffers(List<AirlineFlightOffer> airlineLastMinuteOffers, String airlineName){
         LOGGER.log(Level.INFO, "Save offer of airline {0}", airlineName);
     }
-
-    public void saveUserInterests(InterestsRequest request, String username) 
-                            throws AirportNotFoundException, UserNotFoundException {
-
-        // Check airport
-        Airport airportOut1 = databaseManager.getAirport(request.getOutboundFlight().getArrivalAirportCode());
-        Airport airportOut2 = databaseManager.getAirport(request.getOutboundFlight().getDepartureAirportCode());
-        Airport airportBack1 = null;
-        Airport airportBack2 = null;
-        if(request.getFlightBack() != null){
-            airportBack1 = databaseManager.getAirport(request.getFlightBack().getArrivalAirportCode());
-            airportBack2 = databaseManager.getAirport(request.getFlightBack().getDepartureAirportCode());
-        }
-
-        // Get User
-        User user = databaseManager.getUser(username);
-
-        UserInterest interest = new UserInterest();
-
-        FlightInterest flightOutInterest = new FlightInterest();
-        flightOutInterest.setUser(user);
-        flightOutInterest.setDepartureAirport(airportOut1);
-        flightOutInterest.setArrivalAirport(airportOut2);
-
-        flightOutInterest.setDepartureDateTime(
-            ZonedDateTime.ofInstant(Instant.ofEpochMilli(request.getOutboundFlight().getDepartureTimestamp()), 
-            ZoneId.systemDefault()).toOffsetDateTime());
-        flightOutInterest.setArrivalDateTime(
-            ZonedDateTime.ofInstant(Instant.ofEpochMilli(request.getOutboundFlight().getArrivalTimestamp()), 
-            ZoneId.systemDefault()).toOffsetDateTime());
-        
-        interest.setFlightBackInterest(flightOutInterest);
-
-        if(request.getFlightBack() != null){
-            FlightInterest flightBackInterest = new FlightInterest();
-            flightBackInterest.setUser(user);
-            flightBackInterest.setDepartureAirport(airportBack1);
-            flightBackInterest.setArrivalAirport(airportBack2);
-    
-            flightBackInterest.setDepartureDateTime(
-                ZonedDateTime.ofInstant(Instant.ofEpochMilli(request.getFlightBack().getDepartureTimestamp()), 
-                ZoneId.systemDefault()).toOffsetDateTime());
-                flightBackInterest.setArrivalDateTime(
-                ZonedDateTime.ofInstant(Instant.ofEpochMilli(request.getFlightBack().getArrivalTimestamp()), 
-                ZoneId.systemDefault()).toOffsetDateTime());
-
-            interest.setFlightBackInterest(flightBackInterest);
-        }
-
-        databaseManager.saveUserInterest(interest);
-    }
-
 
     public List<InterestDTO> convertIntToIntDTO(List<FlightInterest> interests) {
         ArrayList<InterestDTO> list = new ArrayList<>();
