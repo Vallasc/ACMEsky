aa118787fd21a16c162f9c4e6e4e056255f19c97
Vallasc <giacomovallorani.97@gmail.com>
2021-07-03 11:36:55 +0200

    Code refactor

--- src/ACMEskyService/src/main/java/it/unibo/soseng/camunda/ProcessState.java
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/camunda/ProcessState.java
@@ -7,58 +7,31 @@ import javax.ejb.ConcurrencyManagement;
 import javax.ejb.Lock;
 import javax.ejb.LockType;
 import javax.ejb.Singleton;
-import javax.ws.rs.container.AsyncResponse;
 
 import static javax.ejb.ConcurrencyManagementType.BEAN;
 
 @Singleton 
 @ConcurrencyManagement(BEAN)
 public class ProcessState {
-    private ConcurrentHashMap<String, AsyncResponseDTO> restResponses;
-    private ConcurrentHashMap<String, String> processStates;
+    private ConcurrentHashMap<String, Object> processStates;
     
     @PostConstruct
     public void init(){
-        restResponses = new ConcurrentHashMap<>();
         processStates = new ConcurrentHashMap<>();
     }
 
     @Lock(LockType.READ)
-    public void setResponse(String username, AsyncResponse response, Object companion){
-        restResponses.put("response-" + username, new AsyncResponseDTO(response, companion));
+    public void setState(String state, String username, String key, Object companion){
+        processStates.put(state + username + key, companion);
     }
 
     @Lock(LockType.READ)
-    public AsyncResponseDTO getResponse(String username){
-        return restResponses.get("response-" + username);
+    public Object getState(String state, String username, String key){
+        return processStates.get(state + username + key);
     }
 
     @Lock(LockType.READ)
-    public void setState(String username, String className){
-        processStates.put("state-" + username, className);
-    }
-
-    @Lock(LockType.READ)
-    public String getState(String username){
-        return processStates.get("state-" + username);
-    }
-
-    public class AsyncResponseDTO {
-        private final AsyncResponse response;
-        private final Object companion;
-
-        AsyncResponseDTO(AsyncResponse response, Object companion){
-            this.response = response;
-            this.companion = companion;
-        }
-
-        public Object getCompanion() {
-            return companion;
-        }
-
-        public AsyncResponse getResponse() {
-            return response;
-        }
-
+    public Object getStateAndRemove(String state, String username, String key){
+        return processStates.remove(state + username + key);
     }
 }
