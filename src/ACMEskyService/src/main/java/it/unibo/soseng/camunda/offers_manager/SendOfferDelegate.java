2f972556f1e20de03099ae4cbc38d3445a212de3
Vallasc <giacomovallorani.97@gmail.com>
2021-07-28 19:07:09 +0200

    Change notification request

--- src/ACMEskyService/src/main/java/it/unibo/soseng/camunda/offers_manager/SendOfferDelegate.java
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/camunda/offers_manager/SendOfferDelegate.java
@@ -5,14 +5,15 @@ import org.camunda.bpm.engine.delegate.JavaDelegate;
 
 import io.jsonwebtoken.io.IOException;
 import it.unibo.soseng.gateway.prontogram.ProntogramClient;
-import it.unibo.soseng.gateway.prontogram.dto.Notification;
+import it.unibo.soseng.gateway.prontogram.dto.NotificationDTO;
 import it.unibo.soseng.model.GeneratedOffer;
 import it.unibo.soseng.model.UserInterest;
 
 import static it.unibo.soseng.camunda.utils.ProcessVariables.GENERATED_OFFER;
 import static it.unibo.soseng.camunda.utils.ProcessVariables.PROCESS_ERROR;
-import static it.unibo.soseng.camunda.utils.ProcessVariables.USER_INTEREST;
+import static it.unibo.soseng.camunda.utils.ProcessVariables.USER_INTERESTS;
 import static it.unibo.soseng.camunda.utils.ProcessVariables.USER_INTEREST_INDEX;
+import static it.unibo.soseng.camunda.utils.ProcessVariables.PRONTOGRAM_USERNAME;
 
 import java.util.List;
 import java.util.logging.Logger;
@@ -23,27 +24,20 @@ import javax.inject.Named;
 
 @Named("sendOfferDelegate")
 public class SendOfferDelegate implements JavaDelegate{
-    private final static Logger LOGGER = Logger.getLogger("sendOfferDelegate"); 
+    private final static Logger LOGGER = Logger.getLogger(SendOfferDelegate.class.getName()); 
+    
     @Inject
     ProntogramClient prontogramClient;
     
     @Override
     public void execute(DelegateExecution execution) throws IOException, InterruptedException, java.io.IOException{
-    LOGGER.info ("sendOfferDelegate in esecuzione");
+    LOGGER.info ("Execute sendOfferDelegate");
 
-    if(execution.getVariable(PROCESS_ERROR) != null){
-      return ;
-    }
+    String prontogramUsername = (String) execution.getVariable(PRONTOGRAM_USERNAME);
+
     GeneratedOffer offer = (GeneratedOffer) execution.getVariable(GENERATED_OFFER);
-    List <UserInterest> userInterests = (List<UserInterest>) execution.getVariable(USER_INTEREST);
-    int userInterestIndex = (int) execution.getVariable(USER_INTEREST_INDEX);
-    String offerToken = String.valueOf(offer.getId());
-    Notification offerToSend = new Notification();
-    offerToSend.setOfferToken(offerToken);
-    offerToSend.setUsername(userInterests.get(userInterestIndex).getUser().getEntity().getUsername());
-    offerToSend.setFlyOutBound(offer.getOutboundFlightId());
-    offerToSend.setFlyBack(offer.getFlightBackId());
-    // prontogramClient.sendNotificationOffer(offerToSend);
-    execution.setVariable(USER_INTEREST_INDEX, userInterestIndex + 1);
+
+    NotificationDTO notification = NotificationDTO.fromOffer(offer, prontogramUsername);
+    prontogramClient.sendNotificationOffer(notification);
   } 
 }
