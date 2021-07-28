617a52c891b82dae026d9c622d273a54b79fd9ba
Vallasc <giacomovallorani.97@gmail.com>
2021-07-28 19:07:52 +0200

    Minor changes

--- src/ACMEskyService/src/main/java/it/unibo/soseng/camunda/offers_manager/InitializeOfferService.java
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/camunda/offers_manager/InitializeOfferService.java
@@ -1,7 +1,6 @@
 package it.unibo.soseng.camunda.offers_manager;
-import static it.unibo.soseng.camunda.utils.ProcessVariables.USER_INTEREST;
+import static it.unibo.soseng.camunda.utils.ProcessVariables.USER_INTERESTS;
 import static it.unibo.soseng.camunda.utils.ProcessVariables.USER_INTEREST_INDEX;
-import static it.unibo.soseng.camunda.utils.ProcessVariables.USER_INTEREST_LEN;
 
 import java.util.List;
 import java.util.logging.Logger;
@@ -12,20 +11,19 @@ import org.camunda.bpm.engine.delegate.JavaDelegate;
 import it.unibo.soseng.logic.database.DatabaseManager;
 import it.unibo.soseng.model.UserInterest;
 
-@Named("initializeFlightInterestServiceDelegate")
+@Named("initializeOfferServiceDelegate")
 public class InitializeOfferService implements JavaDelegate {
-    private final static Logger LOGGER = Logger.getLogger("retrieveFlightsOfInterestDelegate"); 
+    private final static Logger LOGGER = Logger.getLogger(InitializeOfferService.class.getName()); 
     
     @Inject
     DatabaseManager dbManager; 
     
     @Override
     public void execute(DelegateExecution execution) throws Exception {
-        LOGGER.info("retrieveFlightsOfInterestDelegateExecution");
-        List <UserInterest> usersInterests = dbManager.retrieveUserInterests();
-        execution.setVariable(USER_INTEREST_LEN, usersInterests.size());
+        LOGGER.info("Execute InitializeOfferServiceDelegate");
+        List<UserInterest> usersInterests = dbManager.retrieveUserInterests();
         execution.setVariable(USER_INTEREST_INDEX, 0);
-        execution.setVariable(USER_INTEREST, usersInterests);   
+        execution.setVariable(USER_INTERESTS, usersInterests);   
     }
     
 }
--- src/ACMEskyService/src/main/java/it/unibo/soseng/camunda/offers_manager/PrepareOfferDelegate.java
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/camunda/offers_manager/PrepareOfferDelegate.java
@@ -6,10 +6,7 @@ import static it.unibo.soseng.camunda.utils.ProcessVariables.AVAILABLE_FLIGHTS;
 import static it.unibo.soseng.camunda.utils.ProcessVariables.GENERATED_OFFER;
 import static it.unibo.soseng.camunda.utils.ProcessVariables.PROCESS_ERROR;
 import static it.unibo.soseng.camunda.utils.ProcessVariables.USERNAME;
-import static it.unibo.soseng.camunda.utils.ProcessVariables.USER_INTEREST;
-import static it.unibo.soseng.camunda.utils.ProcessVariables.USER_INTEREST_INDEX;
 
-import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -20,25 +17,24 @@ import it.unibo.soseng.logic.database.DatabaseManager.UserNotFoundException;
 import it.unibo.soseng.logic.offer.OfferManager;
 import it.unibo.soseng.model.Flight;
 import it.unibo.soseng.model.GeneratedOffer;
-import it.unibo.soseng.model.UserInterest;
 
 import java.util.logging.Logger;
 
 
 @Named("prepareOfferDelegate")
 public class PrepareOfferDelegate implements JavaDelegate{
-    private final static Logger LOGGER = Logger.getLogger("prepareOfferDelegate"); 
+    private final static Logger LOGGER = Logger.getLogger(PrepareOfferDelegate.class.getName()); 
     
     @Inject
     OfferManager offerManager;
 
     @Override
     public void execute(DelegateExecution execution) {
-      LOGGER.info ("prepareOfferDelegate in esecuzione");
+      LOGGER.info ("Execute prepareOfferDelegate");
+
       @SuppressWarnings (value="unchecked")
       List<Flight> matchedFlights = (List<Flight>) execution.getVariable(AVAILABLE_FLIGHTS);
-      List <UserInterest> userInterests = (List<UserInterest>) execution.getVariable(USER_INTEREST);
-      UserInterest ui = userInterests.get( (int) execution.getVariable(USER_INTEREST_INDEX));
+
       try{
         String username = (String) execution.getVariable(USERNAME);
         GeneratedOffer offer = offerManager.generateOffer(matchedFlights.get(0), matchedFlights.get(1), username);
