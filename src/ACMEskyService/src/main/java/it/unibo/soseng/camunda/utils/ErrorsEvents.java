ba8d871b3eb4973535c10bc84298459d334f0cbb
Vallasc <giacomovallorani.97@gmail.com>
2021-08-11 16:21:51 +0200

    Add prepare offer error

--- src/ACMEskyService/src/main/java/it/unibo/soseng/camunda/utils/ErrorsEvents.java
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/camunda/utils/ErrorsEvents.java
@@ -1,8 +1,9 @@
 package it.unibo.soseng.camunda.utils;
 
 public class ErrorsEvents {
-    public static String SEND_TICKET_ERROR = "sendTicketError";
-    public static String RESPONSE_PAYMENT_LINK_ERROR = "sendTicketError";
+    public static final String SEND_TICKET_ERROR = "sendTicketError";
+    public static final String RESPONSE_PAYMENT_LINK_ERROR = "sendTicketError";
     public static final String DISTANCE_ERROR = "distanceServiceError";
-    
+    public static final String PREPARE_OFFER_ERROR = "prepareOfferError";
+
 }
