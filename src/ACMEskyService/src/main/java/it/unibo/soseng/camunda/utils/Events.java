199a5dac7c80f9b97ab24ef26a5c469a655512b5
Vallasc <giacomovallorani.97@gmail.com>
2021-08-14 16:06:33 +0200

    Add payment successful event

--- src/ACMEskyService/src/main/java/it/unibo/soseng/camunda/utils/Events.java
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/camunda/utils/Events.java
@@ -4,6 +4,5 @@ public final class Events {
     public static final String SAVE_LAST_MINUTE = "StartSaveLastMinute";
     public static final String SAVE_INTERESTS = "SaveInterest";
     public static final String PAY_OFFER = "PayOffer";
-
-    public static final String PAYMENT_REQUEST = "PaymentRequest";
+    public static final String PAYMENT_SUCCESSFUL = "PaymentSuccessful";
 }
