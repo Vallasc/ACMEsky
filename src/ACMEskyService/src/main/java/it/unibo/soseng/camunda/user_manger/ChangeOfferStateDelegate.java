c14be5b59580d3f2f9d6cbe8c4f3168b3071641f
andiub97 <43032657+andiub97@users.noreply.github.com>
2021-08-14 17:49:12 +0200

    introChangeOfferState

--- src/ACMEskyService/src/main/java/it/unibo/soseng/camunda/user_manger/ChangeOfferStateDelegate.java
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/camunda/user_manger/ChangeOfferStateDelegate.java
@@ -2,20 +2,33 @@ package it.unibo.soseng.camunda.user_manger;
 
 import java.util.logging.Logger;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
+import it.unibo.soseng.logic.offer.OfferManager;
+import it.unibo.soseng.model.GeneratedOffer;
+
+import static it.unibo.soseng.camunda.utils.ProcessVariables.USER_OFFER;
+
 @Named("changeOfferStateDelegate")
-public class ChangeOfferStateDelegate implements JavaDelegate{
+public class ChangeOfferStateDelegate implements JavaDelegate {
 
     private final static Logger LOGGER = Logger.getLogger(ChangeOfferStateDelegate.class.getName());
+
+    @Inject
+    OfferManager offerManager;
 
     @Override
     public void execute(DelegateExecution execution) throws Exception {
         LOGGER.info("ChangeOfferStateDelegate working");
-        
+
+        GeneratedOffer offer = ((GeneratedOffer) execution.getVariable(USER_OFFER));
+
+        offerManager.setBookedOffer(offer);
+
     }
-    
+
 }
--- src/ACMEskyService/src/main/java/it/unibo/soseng/logic/offer/OfferManager.java
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/logic/offer/OfferManager.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.ListIterator;
 import java.util.Random;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import java.nio.charset.Charset;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
@@ -38,6 +39,7 @@ import it.unibo.soseng.ws.generated.BookRentResponse;
 import it.unibo.soseng.ws.generated.Rent;
 import it.unibo.soseng.ws.generated.RentService1;
 import it.unibo.soseng.ws.generated.RentService2;
+
 @Stateless
 public class OfferManager {
     private final static Logger LOGGER = Logger.getLogger(OfferManager.class.getName());
@@ -51,32 +53,33 @@ public class OfferManager {
     @Inject
     private RentClient rentClient;
 
-    public GeneratedOffer generateOffer(Flight outBound, Flight flightBack, String username) throws PersistenceException, FlightNotExistException, UserNotFoundException {    
+    public GeneratedOffer generateOffer(Flight outBound, Flight flightBack, String username)
+            throws PersistenceException, FlightNotExistException, UserNotFoundException {
         this.setFlightUnavailable(outBound);
         this.setFlightUnavailable(flightBack);
 
-        GeneratedOffer offerTogenerate = new GeneratedOffer ();
+        GeneratedOffer offerTogenerate = new GeneratedOffer();
         offerTogenerate.setBooked(false);
         offerTogenerate.setOutboundFlightId(outBound);
         offerTogenerate.setFlightBackId(flightBack);
         offerTogenerate.setExpireDate(OffsetDateTime.now().plusHours(24));
         offerTogenerate.setTotalPrice(flightBack.getPrice() + outBound.getPrice());
-        offerTogenerate.setToken(getRandomString(10)); //TODO
+        offerTogenerate.setToken(getRandomString(10)); // TODO
         User user = databaseManager.getUser(username);
         offerTogenerate.setUser(user);
         databaseManager.createOffer(offerTogenerate);
         return offerTogenerate;
     }
 
-    public List<Flight> checkFlightsRequirements(UserInterest ui){
-        List<Flight> matchedFlights = new ArrayList<Flight> ();
+    public List<Flight> checkFlightsRequirements(UserInterest ui) {
+        List<Flight> matchedFlights = new ArrayList<Flight>();
         List<Flight> flights = databaseManager.retrieveFlights();
 
-        for(Flight flight1 : flights){
-            if(this.isMatched(flight1, ui.getOutboundFlightInterest())){
-                for(Flight flight2 : flights){
-                    if(flight1 != flight2 && this.isMatched(flight2, ui.getFlightBackInterest()) &&
-                    flight1.getPrice() + flight2.getPrice() <= ui.getPriceLimit()){
+        for (Flight flight1 : flights) {
+            if (this.isMatched(flight1, ui.getOutboundFlightInterest())) {
+                for (Flight flight2 : flights) {
+                    if (flight1 != flight2 && this.isMatched(flight2, ui.getFlightBackInterest())
+                            && flight1.getPrice() + flight2.getPrice() <= ui.getPriceLimit()) {
                         matchedFlights.add(flight1);
                         matchedFlights.add(flight2);
                         return matchedFlights;
@@ -88,31 +91,45 @@ public class OfferManager {
         return matchedFlights;
     }
 
-    private boolean isMatched (Flight f, FlightInterest fi) {
+    private boolean isMatched(Flight f, FlightInterest fi) {
         OffsetDateTime flightDate = f.getDepartureDateTime();
         OffsetDateTime interestDateStart = fi.getDepartureDateTime();
         OffsetDateTime interestDateEnd = interestDateStart.plusDays(1);
 
-        return f.getDepartureAirport() == fi.getDepartureAirport() && 
-                f.getArrivalAirport() == fi.getArrivalAirport() &&
-                interestDateStart.compareTo(flightDate) <=0 && 
-                interestDateEnd.compareTo(flightDate) >=0;
+        return f.getDepartureAirport() == fi.getDepartureAirport() && f.getArrivalAirport() == fi.getArrivalAirport()
+                && interestDateStart.compareTo(flightDate) <= 0 && interestDateEnd.compareTo(flightDate) >= 0;
     }
 
-    public void setFlightUnavailable(Flight f){
+    public void setFlightUnavailable(Flight f) {
         f.setAvailable(false);
         databaseManager.updateFlight(f);
     }
 
-    public void removeExpiredOffers() {
+    public List<GeneratedOffer> removeExpiredOffers() {
         OffsetDateTime now = OffsetDateTime.now();
-        List<GeneratedOffer> expFlights = databaseManager.getAvailableFlights().stream().dropWhile(f -> f.getExpireDate().isBefore(now)).collect(Collectors.toList());
-        for (ListIterator<Flight> iter = expFlights.listIterator(); iter.hasNext(); ) {
-            Flight f = iter.next();
-            f.setAvailable(false);
-            databaseManager.updateFlight(f);
+        ArrayList<GeneratedOffer> offersNotAvailable = new ArrayList<>();
+        List<GeneratedOffer> expFlights = databaseManager.getAvailableOffers().stream()
+                .dropWhile(f -> f.getExpireDate().isBefore(now)).collect(Collectors.toList());
+        for (ListIterator<GeneratedOffer> iter = expFlights.listIterator(); iter.hasNext();) {
+            GeneratedOffer g = iter.next();
+            // g.setAvailable(false);
+            offersNotAvailable.add(g);
+            databaseManager.updateGeneratedOffer(g);
+
         }
-    } 
+
+        return offersNotAvailable;
+    }
+
+    public void setUnavailableOfferFlights(List<GeneratedOffer> list) {
+        for (ListIterator<GeneratedOffer> iter = list.listIterator(); iter.hasNext();) {
+            GeneratedOffer g = iter.next();
+            g.getOutboundFlightId().setAvailable(true);
+            g.getFlightBackId().setAvailable(true);
+            databaseManager.updateFlight(g.getOutboundFlightId());
+            databaseManager.updateFlight(g.getFlightBackId());
+        }
+    }
 
     public class SendTicketException extends Exception {
         private static final long serialVersionUID = 1L;
@@ -120,15 +137,18 @@ public class OfferManager {
 
     /**
      * Ritona la distanza tra l'aereoporto dell'offerta e línidirizzo inserito
+     * 
      * @throws DistanceServiceException
      */
     public float getDistance(AddressDTO userAddress, GeneratedOffer offer) throws DistanceServiceException {
         Float latitude = offer.getOutboundFlightId().getDepartureAirport().getLatitude();
         Float longitude = offer.getOutboundFlightId().getDepartureAirport().getLongitude();
-        String fullAddress = userAddress.getAddress() + ", " + userAddress.getPostCode() + " " + userAddress.getCityName() + ", " + userAddress.getCountry();
+        String fullAddress = userAddress.getAddress() + ", " + userAddress.getPostCode() + " "
+                + userAddress.getCityName() + ", " + userAddress.getCountry();
         try {
-            DistanceDTO distance = distanceClient.requestDistance(fullAddress , latitude.toString() + ',' + longitude.toString());
-            if(!distance.getStatus().equals("OK"))
+            DistanceDTO distance = distanceClient.requestDistance(fullAddress,
+                    latitude.toString() + ',' + longitude.toString());
+            if (!distance.getStatus().equals("OK"))
                 throw new DistanceServiceException();
             return distance.getValue();
         } catch (IOException | GeoserverErrorException e) {
@@ -137,11 +157,13 @@ public class OfferManager {
         }
     }
 
-    public void bookAllRent(String email, AddressDTO address, GeneratedOffer offer, DelegateExecution execution) throws UserNotFoundException {
+    public void bookAllRent(String email, AddressDTO address, GeneratedOffer offer, DelegateExecution execution)
+            throws UserNotFoundException {
         User user = databaseManager.getUser(email);
-        String userAddress = address.getAddress() + ", " + address.getPostCode() + " " + address.getCityName() + ", " + address.getCountry();
+        String userAddress = address.getAddress() + ", " + address.getPostCode() + " " + address.getCityName() + ", "
+                + address.getCountry();
         Airport airport = offer.getOutboundFlightId().getDepartureAirport();
-        String airportAddress = String.valueOf(airport.getLatitude()) +','+ String.valueOf(airport.getLongitude());
+        String airportAddress = String.valueOf(airport.getLatitude()) + ',' + String.valueOf(airport.getLongitude());
         try {
             BookRentResponse reponse = this.bookRent(RentService1.class, user, userAddress, airportAddress);
             execution.setVariable(RENT_OUTBOUND, reponse);
@@ -156,17 +178,18 @@ public class OfferManager {
         }
     }
 
-    public BookRentResponse bookRent(Class service, User user, String addressFrom, String addressTo) throws RentServiceException {
+    public BookRentResponse bookRent(Class service, User user, String addressFrom, String addressTo)
+            throws RentServiceException {
         RentService1 rentService1 = new RentService1();
         RentService2 rentService2 = new RentService2();
         Rent ws;
-        if( service.getName() == "RentService1" )
+        if (service.getName() == "RentService1")
             ws = rentService1.getRentServicePort();
         else
             ws = rentService2.getRentServicePort();
 
         BookRent bookRent = new BookRent();
-        bookRent.setClientName(user.getName()); 
+        bookRent.setClientName(user.getName());
         bookRent.setClientSurname(user.getSurname());
         bookRent.setFromAddress(addressFrom);
         bookRent.setToAddress(addressTo);
@@ -174,47 +197,51 @@ public class OfferManager {
         return rentClient.rentRequest(ws, bookRent);
     }
 
-    public class DistanceServiceException extends Exception {}
+    public class DistanceServiceException extends Exception {
+    }
 
-    public class RentServiceException extends Exception {}
-   
-    static String getRandomString(int i) 
-    { 
-        // bind the length 
+    public class RentServiceException extends Exception {
+    }
+
+    static String getRandomString(int i) {
+        // bind the length
         byte[] bytearray;
-        bytearray = new byte[256];         
+        bytearray = new byte[256];
         String mystring;
         StringBuffer thebuffer;
         String theAlphaNumericS;
 
-        new Random().nextBytes(bytearray); 
+        new Random().nextBytes(bytearray);
 
-        mystring 
-            = new String(bytearray, Charset.forName("UTF-8")); 
-            
+        mystring = new String(bytearray, Charset.forName("UTF-8"));
+
         thebuffer = new StringBuffer();
-        
-        //remove all spacial char 
-        theAlphaNumericS 
-            = mystring 
-                .replaceAll("[^A-Z0-9]", ""); 
 
-        //random selection
-        for (int m = 0; m < theAlphaNumericS.length(); m++) { 
+        // remove all spacial char
+        theAlphaNumericS = mystring.replaceAll("[^A-Z0-9]", "");
 
-            if (Character.isLetter(theAlphaNumericS.charAt(m)) 
-                    && (i > 0) 
-                || Character.isDigit(theAlphaNumericS.charAt(m)) 
-                    && (i > 0)) { 
+        // random selection
+        for (int m = 0; m < theAlphaNumericS.length(); m++) {
 
-                thebuffer.append(theAlphaNumericS.charAt(m)); 
-                i--; 
-            } 
-        } 
+            if (Character.isLetter(theAlphaNumericS.charAt(m)) && (i > 0)
+                    || Character.isDigit(theAlphaNumericS.charAt(m)) && (i > 0)) {
 
-        // the resulting string 
-        return thebuffer.toString(); 
+                thebuffer.append(theAlphaNumericS.charAt(m));
+                i--;
+            }
+        }
+
+        // the resulting string
+        return thebuffer.toString();
+    }
+
+    public void setBookedOffer(GeneratedOffer offer) {
+
+        offer.setBooked(true);
+        // offer.setAvailable(false);
+        databaseManager.updateGeneratedOffer(offer);
+
+        databaseManager.setBookFlights(true, offer.getOutboundFlightId(), offer.getFlightBackId());
     }
 
 }
-
