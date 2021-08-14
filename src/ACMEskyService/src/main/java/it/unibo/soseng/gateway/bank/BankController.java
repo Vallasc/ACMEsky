c18f5367729eddc197f27195749cec621e3b4e45
Vallasc <giacomovallorani.97@gmail.com>
2021-08-14 16:09:52 +0200

    Add bank manager

--- src/ACMEskyService/src/main/java/it/unibo/soseng/gateway/bank/BankController.java
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/gateway/bank/BankController.java
@@ -1,22 +1,37 @@
 package it.unibo.soseng.gateway.bank;
 
 import javax.ws.rs.Path;
-
-//import org.camunda.bpm.engine.ProcessEngines;
-//import org.camunda.bpm.engine.RuntimeService;
-
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import it.unibo.soseng.logic.bank.BankManager;
 
 import java.util.logging.Logger;
 
+import static it.unibo.soseng.security.Constants.BANK;
 
-@Path("bank")
+@Path("banks")
+@RolesAllowed({BANK})
+@Produces( MediaType.APPLICATION_JSON )
 public class BankController {
     private final static Logger LOGGER = Logger.getLogger(BankController.class.getName());
 
+    @Inject
+    BankManager bankManager;
+
     @GET
-    @Path("hello")
-    public String helloworld() {
-        return "Hello World!";
+    @PermitAll
+    @Path("/confirmPayment")
+    public Response confirmPayment(@QueryParam("code") String code) {
+        LOGGER.info("GET confirmPayment");
+        LOGGER.info("code "+code);
+        bankManager.payentSuccess(code);
+        return Response.status(Response.Status.OK.getStatusCode()).build();
     }
 }
