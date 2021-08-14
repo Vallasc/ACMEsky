81798e5b5812b504bcd43e1daf23b87bd1dc1db4
andiub97 <43032657+andiub97@users.noreply.github.com>
2021-08-14 17:48:15 +0200

    removeOffers

--- src/ACMEskyService/src/main/java/it/unibo/soseng/camunda/offers_manager/remove_expired_offers/RemoveExpiredOffers.java
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/camunda/offers_manager/remove_expired_offers/RemoveExpiredOffers.java
@@ -1,16 +1,20 @@
 package it.unibo.soseng.camunda.offers_manager.remove_expired_offers;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
 import it.unibo.soseng.logic.offer.OfferManager;
+import it.unibo.soseng.model.GeneratedOffer;
 
+@Named("removeExpiredOffersDelegate")
 public class RemoveExpiredOffers implements JavaDelegate {
-    
+
     private final static Logger LOGGER = Logger.getLogger(RemoveExpiredOffers.class.getName());
 
     @Inject
@@ -19,7 +23,9 @@ public class RemoveExpiredOffers implements JavaDelegate {
     @Override
     public void execute(DelegateExecution execution) throws Exception {
         LOGGER.info("RemoveExpiredOffers is working");
-        offerManager.removeExpiredOffers();
+        List<GeneratedOffer> offersNotAvailable = offerManager.removeExpiredOffers();
+        offerManager.setUnavailableOfferFlights(offersNotAvailable);
+
     }
 
 }
