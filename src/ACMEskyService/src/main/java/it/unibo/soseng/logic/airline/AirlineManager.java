cfb6c2a918a9e66d9b78a13d23c7a736024ad6e1
andiub97 <43032657+andiub97@users.noreply.github.com>
2021-08-14 19:32:40 +0200

    fixSendTicketException

--- src/ACMEskyService/src/main/java/it/unibo/soseng/logic/airline/AirlineManager.java
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/logic/airline/AirlineManager.java
@@ -28,7 +28,6 @@ import it.unibo.soseng.gateway.airline.dto.InterestDTO;
 import it.unibo.soseng.logic.database.DatabaseManager;
 import it.unibo.soseng.logic.database.DatabaseManager.AirlineNotFoundException;
 import it.unibo.soseng.logic.database.DatabaseManager.AirportNotFoundException;
-import it.unibo.soseng.logic.offer.OfferManager.SendTicketException;
 import it.unibo.soseng.model.Flight;
 import it.unibo.soseng.model.FlightInterest;
 import it.unibo.soseng.model.GeneratedOffer;
@@ -57,32 +56,30 @@ public class AirlineManager {
     private AirlineClient client;
 
     // Camunda
-    public void startSaveLastMinuteOffer(List<AirlineFlightOfferDTO> request) 
-    throws BadRequestException{
+    public void startSaveLastMinuteOffer(List<AirlineFlightOfferDTO> request) throws BadRequestException {
         LOGGER.info("StartSaveLastMinuteProcess");
         String airlineCompanyName = securityContext.getCallerPrincipal().getName();
         final RuntimeService runtimeService = ProcessEngines.getDefaultProcessEngine().getRuntimeService();
-        Map<String,Object> processVariables = new HashMap<String,Object>();
+        Map<String, Object> processVariables = new HashMap<String, Object>();
 
         processVariables.put(AIRLINE_FLIGHT_OFFERS, request);
-        processVariables.put(AIRLINE_NAME, airlineCompanyName);  
+        processVariables.put(AIRLINE_NAME, airlineCompanyName);
         // Start the process instance
         ProcessInstanceWithVariables instance = runtimeService.createProcessInstanceByKey(SAVE_LAST_MINUTE)
-                                                                .setVariables(processVariables)
-                                                                .executeWithVariablesInReturn();
+                .setVariables(processVariables).executeWithVariablesInReturn();
         processVariables = instance.getVariables();
         String error = (String) processVariables.get(PROCESS_ERROR);
-        if(error != null)
+        if (error != null)
             throw new BadRequestException();
-        
+
     }
 
-    public void handleConfirmLastMinuteOffer(String airlineName, List<AirlineFlightOfferDTO>  airlineOffers) {
+    public void handleConfirmLastMinuteOffer(String airlineName, List<AirlineFlightOfferDTO> airlineOffers) {
         // Control date time
         List<Flight> flights = new ArrayList<>();
-        for (int i = 0; i < airlineOffers.size (); i++) {
+        for (int i = 0; i < airlineOffers.size(); i++) {
             try {
-                Flight flight = new Flight ();
+                Flight flight = new Flight();
                 flight.setAirline(databaseManager.getAirline(airlineName));
                 flight.setArrivalAirport(databaseManager.getAirport(airlineOffers.get(i).getArrivalAirportCode()));
                 flight.setDepartureAirport(databaseManager.getAirport(airlineOffers.get(i).getDepartureAirportCode()));
@@ -96,30 +93,28 @@ public class AirlineManager {
                 // Control date time
                 OffsetDateTime now = OffsetDateTime.now();
 
-                if(flight.getExpireDate().compareTo(now) > 0 ) {
-                   flights.add(flight);
+                if (flight.getExpireDate().compareTo(now) > 0) {
+                    flights.add(flight);
                 }
-                
-                
-  
-            } catch (AirportNotFoundException | AirlineNotFoundException e1 ) {
+
+            } catch (AirportNotFoundException | AirlineNotFoundException e1) {
                 LOGGER.warning(e1.toString());
-            } 
+            }
 
         }
 
-        databaseManager.insertFlights(flights);    
+        databaseManager.insertFlights(flights);
     }
-    public void saveAirlineOffers(List<AirlineFlightOfferDTO> airlineLastMinuteOffers, String airlineName){
+
+    public void saveAirlineOffers(List<AirlineFlightOfferDTO> airlineLastMinuteOffers, String airlineName) {
         LOGGER.log(Level.INFO, "Save offer of airline {0}", airlineName);
     }
 
     public List<InterestDTO> convertIntToIntDTO(List<FlightInterest> interests) {
         ArrayList<InterestDTO> list = new ArrayList<>();
-        for(FlightInterest i : interests){
-            InterestDTO dto = new InterestDTO(i.getDepartureAirport().getAirportCode(), 
-                                            i.getArrivalAirport().getAirportCode(),
-                                            i.getDepartureDateTime().toString()); 
+        for (FlightInterest i : interests) {
+            InterestDTO dto = new InterestDTO(i.getDepartureAirport().getAirportCode(),
+                    i.getArrivalAirport().getAirportCode(), i.getDepartureDateTime().toString());
             list.add(dto);
         }
         return list;
@@ -128,31 +123,31 @@ public class AirlineManager {
     public List<Flight> retrieveFlightsList(List<InterestDTO> listDTO, String wsAddress) {
         List<Flight> list = new ArrayList<>();
         ObjectMapper mapper = new ObjectMapper();
-        try{
+        try {
             String resp = client.getFlightList(listDTO, wsAddress);
             JsonNode root = mapper.readTree(resp);
-            for(JsonNode node: root){
+            for (JsonNode node : root) {
                 Flight f = new Flight();
-                try{
-                f.setDepartureAirport(databaseManager.getAirport(node.get("departureCode").textValue()));
+                try {
+                    f.setDepartureAirport(databaseManager.getAirport(node.get("departureCode").textValue()));
 
-                OffsetDateTime departureDateTime = OffsetDateTime.parse(node.get("departureTime").textValue());
-                f.setDepartureDateTime(departureDateTime);
-                f.setArrivalAirport(databaseManager.getAirport(node.get("arrivalCode").textValue()));
+                    OffsetDateTime departureDateTime = OffsetDateTime.parse(node.get("departureTime").textValue());
+                    f.setDepartureDateTime(departureDateTime);
+                    f.setArrivalAirport(databaseManager.getAirport(node.get("arrivalCode").textValue()));
 
-                OffsetDateTime arrivalDateTime = OffsetDateTime.parse(node.get("arrivalTime").textValue());
-                f.setArrivalDateTime(arrivalDateTime);
-                f.setAirline(databaseManager.getAirline(node.get("airline_id").textValue()));
-                f.setPrice(node.get("price").floatValue());
-                OffsetDateTime expireDateTime = OffsetDateTime.parse(node.get("expDate").textValue());
-                f.setExpireDate(expireDateTime);
-                f.setBooked(false);
-                f.setAvailable(true);
-                f.setFlightCode(node.get("id").asText());
-                list.add(f);
+                    OffsetDateTime arrivalDateTime = OffsetDateTime.parse(node.get("arrivalTime").textValue());
+                    f.setArrivalDateTime(arrivalDateTime);
+                    f.setAirline(databaseManager.getAirline(node.get("airline_id").textValue()));
+                    f.setPrice(node.get("price").floatValue());
+                    OffsetDateTime expireDateTime = OffsetDateTime.parse(node.get("expDate").textValue());
+                    f.setExpireDate(expireDateTime);
+                    f.setBooked(false);
+                    f.setAvailable(true);
+                    f.setFlightCode(node.get("id").asText());
+                    list.add(f);
                 } catch (AirportNotFoundException e) {
-                    LOGGER.info("Airport not found: "+
-                    node.get("departureCode").textValue()+" or " + node.get("arrivalCode").textValue());
+                    LOGGER.info("Airport not found: " + node.get("departureCode").textValue() + " or "
+                            + node.get("arrivalCode").textValue());
                 } catch (AirlineNotFoundException e) {
                     LOGGER.info("Airline not found: " + node.get("airline_id").textValue());
                 }
@@ -165,21 +160,20 @@ public class AirlineManager {
 
     public void removeExpiredFlights() {
         OffsetDateTime now = OffsetDateTime.now();
-        List<Flight> expFlights = databaseManager.getAvailableFlights().stream().dropWhile(f -> f.getExpireDate().isBefore(now)).collect(Collectors.toList());
-        for (ListIterator<Flight> iter = expFlights.listIterator(); iter.hasNext(); ) {
+        List<Flight> expFlights = databaseManager.getAvailableFlights().stream()
+                .dropWhile(f -> f.getExpireDate().isBefore(now)).collect(Collectors.toList());
+        for (ListIterator<Flight> iter = expFlights.listIterator(); iter.hasNext();) {
             Flight f = iter.next();
             f.setAvailable(false);
             databaseManager.updateFlight(f);
         }
     }
 
-    public byte[] bookOfferTicket(GeneratedOffer offer) throws IOException, SendTicketException{
+    public byte[] bookOfferTicket(GeneratedOffer offer) throws IOException, SendTicketException {
 
-        byte[] pdfTicket = client.getFlightTickets(
-                                    offer.getOutboundFlight().getAirlineId().getWsAddress(), 
-                                    offer.getUser().getProntogramUsername(), 
-                                    offer.getOutboundFlight().getFlightCode(), 
-                                    offer.getFlightBack().getFlightCode());
+        byte[] pdfTicket = client.getFlightTickets(offer.getOutboundFlight().getAirlineId().getWsAddress(),
+                offer.getUser().getProntogramUsername(), offer.getOutboundFlight().getFlightCode(),
+                offer.getFlightBack().getFlightCode());
         offer.setBooked(true);
         offer.getOutboundFlight().setBooked(true);
         offer.getFlightBack().setBooked(true);
@@ -189,14 +183,13 @@ public class AirlineManager {
         return pdfTicket;
     }
 
-
     public void unbookOffer(GeneratedOffer offer) throws IOException {
-        
+
         client.unbookFlights(offer);
         offer.setBooked(false);
         offer.getOutboundFlight().setBooked(false);
         offer.getFlightBack().setBooked(false);
-        //TODO fare richiesta fittizia ad airline
+        // TODO fare richiesta fittizia ad airline
     }
 
     public class UserNotAllowedException extends Exception {
@@ -207,6 +200,8 @@ public class AirlineManager {
         private static final long serialVersionUID = 1L;
     }
 
-    
+    public class SendTicketException extends Exception {
+        private static final long serialVersionUID = 1L;
+    }
 
 }
