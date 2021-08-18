234515001ce6be2efeb354487749e22d7380ca83
Riccardo Baratin <babastreio@gmail.com>
2021-08-18 12:13:45 +0200

    update users

--- src/Prontogram/front-end/src/app/account/login.component.ts
+++ src/Prontogram/front-end/src/app/account/login.component.ts
@@ -2,9 +2,10 @@
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
-
 import { AccountService, AlertService, NotificationService } from '../_services';
+import { User} from '../_models';
 import { SwPush } from '@angular/service-worker';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Component({ templateUrl: 'login.component.html' })
 export class LoginComponent implements OnInit {
@@ -12,6 +13,7 @@ export class LoginComponent implements OnInit {
     loading = false;
     submitted = false;
     readonly VAPID_PUBLIC_KEY = "BA161ZnkX9G6CwYOZifUyGpOxslxcANly0PfMtti7y1rDO9NZlPNI1yepdaTodQXX0gVHqXHVApmArL1MUNsBoM";
+
     constructor(
         private formBuilder: FormBuilder,
         private route: ActivatedRoute,
@@ -20,7 +22,27 @@ export class LoginComponent implements OnInit {
         private alertService: AlertService,
         private notificationService: NotificationService,
         private swPush: SwPush
-    ) { }
+    ) 
+    { 
+            let user1 = new User ();
+            user1.name = "riccardo"
+            user1.password = "riccardo"
+            user1.secondName = "baratin"
+            user1.username = "riccardo"
+            this.accountService.register(user1).subscribe(x=>console.log(x),err=>console.log(err));
+            let user2 = new User ();
+            user2.name = "giacomo"
+            user2.password = "giacomo"
+            user2.secondName = "vallorani"
+            user2.username = "giacomo"
+            this.accountService.register(user2).subscribe(x=>console.log(x),err=>console.log(err));
+            let user3 = new User ();
+            user3.name = "andrea"
+            user3.password = "andrea"
+            user3.secondName = "di ubaldo"
+            user3.username = "andrea"
+            this.accountService.register(user3).subscribe(x=>console.log(x),err=>console.log(err)); 
+    }
 
     ngOnInit() {
         this.form = this.formBuilder.group({
