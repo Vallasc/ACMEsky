6bc9496580014232b4576b49e97ae26e72b2207f
andiub97 <43032657+andiub97@users.noreply.github.com>
2021-08-18 22:35:45 +0200

    updOfferCreationAndSearch

--- src/AirlineService/src/main/java/it/soseng/unibo/airlineService/repository/FlightOfferRepository.java
+++ src/AirlineService/src/main/java/it/soseng/unibo/airlineService/repository/FlightOfferRepository.java
@@ -10,13 +10,15 @@ import it.soseng.unibo.airlineService.model.FlightOffer;
 
 /**
  * Quest'interfaccia gestisce la tabella FLIGHT_OFFERS
- * @author Andrea Di Ubaldo
- * andrea.diubaldo@studio.unibo.it
+ * 
+ * @author Andrea Di Ubaldo andrea.diubaldo@studio.unibo.it
  */
 public interface FlightOfferRepository extends JpaRepository<FlightOffer, Long> {
 
-    //@Query(value = "SELECT * FROM FLIGHT_OFFERS o WHERE o.DEPARTURE_AIRPORT_CODE = ?1 AND O.ARRIVAL_AIRPORT_CODE = ?2 AND o.DEPARTURE_DATE_TIME >= ?3 AND o.DEPARTURE_DATE_TIME < ?4 ", nativeQuery = true)
+    // @Query(value = "SELECT * FROM FLIGHT_OFFERS o WHERE o.DEPARTURE_AIRPORT_CODE
+    // = ?1 AND O.ARRIVAL_AIRPORT_CODE = ?2 AND o.DEPARTURE_DATE_TIME >= ?3 AND
+    // o.DEPARTURE_DATE_TIME < ?4 ", nativeQuery = true)
     @Query(value = "SELECT * FROM FLIGHT_OFFERS o WHERE o.DEPARTURE_AIRPORT_CODE = ?1 AND O.ARRIVAL_AIRPORT_CODE = ?2 ", nativeQuery = true)
-    public ArrayList<FlightOffer> searchFlightOffers(String departure, String arrival, OffsetDateTime startDateInterval, OffsetDateTime FinishDateInterval);
+    public ArrayList<FlightOffer> searchFlightOffers(String departure, String arrival);
 
 }
