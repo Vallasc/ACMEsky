68672e92685a2b6846c14b7245917b26938702c7
Vallasc <giacomovallorani.97@gmail.com>
2021-08-19 13:29:31 +0200

    Add get tickets route

--- src/ACMEskyService/src/main/java/it/unibo/soseng/gateway/user/OfferController.java
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/gateway/user/OfferController.java
@@ -1,15 +1,17 @@
 package it.unibo.soseng.gateway.user;
 
+import java.io.File;
 import java.util.logging.Logger;
 
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
+import javax.security.enterprise.SecurityContext;
 import javax.validation.Valid;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
@@ -18,8 +20,12 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
+import it.unibo.soseng.gateway.user.dto.AddressDTO;
 import it.unibo.soseng.gateway.user.dto.UserOfferDTO;
+import it.unibo.soseng.logic.database.DatabaseManager;
+import it.unibo.soseng.logic.database.DatabaseManager.OfferNotFoundException;
 import it.unibo.soseng.logic.offer.OfferManager;
+import it.unibo.soseng.model.GeneratedOffer;
 
 import static it.unibo.soseng.security.Constants.USER;
 
@@ -30,6 +36,12 @@ public class OfferController {
 
     @Inject
     private OfferManager offerManager;
+
+    @Inject
+    private DatabaseManager databaseManager;
+
+    @Inject
+    private SecurityContext securityContext;
     
     @PUT
     @Path("/confirm")
@@ -43,26 +55,34 @@ public class OfferController {
     }
 
 
-    @GET
+    @PUT
     @Path("/paymentLink")
     @RolesAllowed({USER})
+    @Consumes( MediaType.APPLICATION_JSON )
     @Produces(MediaType.APPLICATION_JSON)
-    public void requestPaymentLink(final @Suspended AsyncResponse response) {
-        LOGGER.info("GET paymentLink");
-        offerManager.startPaymentRequest(response);
+    public void requestPaymentLink(final @Valid AddressDTO address,
+                                    final @Suspended AsyncResponse response) {
+        LOGGER.info("PUT paymentLink");
+        offerManager.startPaymentRequest(address, response);
     }
     
     @GET
-    @Path("/getTickets")
+    @Path("{token}")
     @RolesAllowed({USER})
-    public Response getTickets() {
-        /*System.out.println(username);
-        byte[] out = (byte[]) processState.getState(PROCESS_BUY_OFFER, username, "PDF");
-        System.out.println(out);
-        return Response.status(Response.Status.OK.getStatusCode())
-                        .header("Content-Type", "application/pdf")
-                        .entity(out)
-                        .build();*/
-        return Response.status(Response.Status.OK.getStatusCode()).build();
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    public Response getTickets(final @PathParam("token") String token) {
+        String email = securityContext.getCallerPrincipal().getName();
+        try {
+            GeneratedOffer offer = databaseManager.getOfferByTokenEmail(token, email);
+            if(offer.getBooked()){ 
+                File file = new File(offer.getToken() + ".pdf");
+                return Response.ok(file, MediaType.APPLICATION_OCTET_STREAM)
+                    .header("Content-Disposition", "attachment; filename=\"" + file.getName() + "\"" )
+                    .build();
+            }
+            return Response.status(Response.Status.NOT_FOUND.getStatusCode()).build();
+        } catch (OfferNotFoundException e) {}
+        return Response.status(Response.Status.NOT_FOUND.getStatusCode()).build();
     }
+
 }
