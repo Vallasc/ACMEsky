46d88b865cc38ffd080dcf6e64af8cfa2751f61a
Vallasc <giacomovallorani.97@gmail.com>
2021-08-19 13:28:26 +0200

    Add bookRent

--- src/ACMEskyService/src/main/java/it/unibo/soseng/gateway/rent/RentClient.java
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/gateway/rent/RentClient.java
@@ -4,9 +4,12 @@ import java.util.logging.Logger;
 
 import javax.ejb.Stateless;
 
+import it.unibo.soseng.model.User;
 import it.unibo.soseng.ws.generated.BookRent;
 import it.unibo.soseng.ws.generated.BookRentResponse;
 import it.unibo.soseng.ws.generated.Rent;
+import it.unibo.soseng.ws.generated.RentService1;
+import it.unibo.soseng.ws.generated.RentService2;
 
 @Stateless
 public class RentClient {
@@ -14,5 +17,29 @@ public class RentClient {
 
     public BookRentResponse rentRequest(Rent rentService, BookRent request) {
         return rentService.bookRent(request);
+    }
+
+    public BookRentResponse bookRent(String service, User user, String addressFrom, String addressTo, 
+                                                                                    String arrivalDateTime){
+        RentService1 rentService1;
+        RentService2 rentService2;
+        Rent ws;
+
+        if( service == "RentGood" ){
+            rentService1 = new RentService1();
+            ws = rentService1.getRentServicePort();
+        } else {
+            rentService2 = new RentService2();
+            ws = rentService2.getRentServicePort();
+        }
+
+        BookRent bookRent = new BookRent();
+        bookRent.setClientName(user.getName()); 
+        bookRent.setClientSurname(user.getSurname());
+        bookRent.setFromAddress(addressFrom);
+        bookRent.setToAddress(addressTo);
+        bookRent.setArrivalDateTime(arrivalDateTime);
+
+        return rentRequest(ws, bookRent);
     }
 }
