46498a6fc6ad329b07fc1984a3a8a77db5dddcfc
Vallasc <giacomovallorani.97@gmail.com>
2021-08-21 09:48:56 +0200

    Initial commit

---
+++ src/ACMEskyService/src/main/java/it/unibo/soseng/gateway/user/dto/FlightDTO.java
@@ -0,0 +1,109 @@
+package it.unibo.soseng.gateway.user.dto;
+
+import java.io.Serializable;
+import java.time.OffsetDateTime;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import it.unibo.soseng.model.Flight;
+
+public class FlightDTO implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+
+    private String flightCode;
+    private String departureAirportCode;
+    private String arrivalAirportCode;
+    private String departureTimestamp;
+    private String arrivalTimestamp;
+    private float price;
+    private String airlineName;
+
+    public String getFlightCode() {
+        return flightCode;
+    }
+
+    public void setFlightCode(String flightCode) {
+        this.flightCode = flightCode;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getArrivalTimestamp() {
+        return arrivalTimestamp;
+    }
+
+    public void setArrivalTimestamp(String arrivalTimestamp) {
+        this.arrivalTimestamp = arrivalTimestamp;
+    }
+
+    public String getDepartureTimestamp() {
+        return departureTimestamp;
+    }
+
+    public void setDepartureTimestamp(String departureTimestamp) {
+        this.departureTimestamp = departureTimestamp;
+    }
+
+    public String getArrivalAirportCode() {
+        return arrivalAirportCode;
+    }
+
+    public void setArrivalAirportCode(String arrivalAirportCode) {
+        this.arrivalAirportCode = arrivalAirportCode;
+    }
+
+    public String getDepartureAirportCode() {
+        return departureAirportCode;
+    }
+
+    public void setDepartureAirportCode(String departureAirportCode) {
+        this.departureAirportCode = departureAirportCode;
+    }
+
+    public String getAirlineName() {
+        return this.airlineName;
+    }
+
+    public void setAirlinename(String airlineName) {
+        this.airlineName = airlineName;
+    }
+
+    @JsonIgnore
+    public OffsetDateTime getDepartureOffsetDateTime () {
+        return OffsetDateTime.parse(this.departureTimestamp); 
+    }
+
+    @JsonIgnore
+    public OffsetDateTime getArrivalOffsetDateTime () {
+        return OffsetDateTime.parse(this.arrivalTimestamp); 
+    }
+
+    @JsonIgnore
+    public void setDepartureOffsetDateTime (OffsetDateTime departurDateTime) {
+        this.departureTimestamp = departurDateTime.toString();
+    }
+
+    @JsonIgnore
+    public void setArrivalOffsetDateTime (OffsetDateTime arrivalDateTime) {
+        this.arrivalTimestamp = arrivalDateTime.toString();
+    }
+
+    public static FlightDTO fromFlight(Flight flight){
+        FlightDTO dto = new FlightDTO();
+        dto.airlineName = flight.getAirlineId().getEntity().getUsername();
+        dto.arrivalAirportCode = flight.getArrivalAirport().getAirportCode();
+        dto.departureAirportCode = flight.getDepartureAirport().getAirportCode();
+        dto.flightCode = flight.getFlightCode();
+        dto.price = flight.getPrice();
+        dto.setDepartureOffsetDateTime(flight.getDepartureDateTime());
+        dto.setArrivalOffsetDateTime(flight.getArrivalDateTime());
+        return dto;
+    }
+}
