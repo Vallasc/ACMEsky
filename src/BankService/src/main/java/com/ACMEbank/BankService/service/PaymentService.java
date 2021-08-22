2272399c1d64b1b4fa9db267bb08419aa647f4fa
Vallasc <giacomovallorani.97@gmail.com>
2021-08-22 12:36:10 +0200

    Add ACMEsky auth, implement sendPaymentNotification

--- src/BankService/src/main/java/com/ACMEbank/BankService/service/PaymentService.java
+++ src/BankService/src/main/java/com/ACMEbank/BankService/service/PaymentService.java
@@ -1,17 +1,24 @@
 package com.ACMEbank.BankService.service;
 
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.io.IOException;
 import java.util.*;
 
+import com.ACMEbank.BankService.dto.AcmeskyAuthRequestDTO;
+import com.ACMEbank.BankService.dto.AcmeskyAuthResponseDTO;
 import com.ACMEbank.BankService.dto.PaymentLinkRequestDTO;
 import com.ACMEbank.BankService.model.WaitingPayment;
 import com.ACMEbank.BankService.repository.WaitingPaymentRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /**
  * Servizio che gestisce i link di pagamento
@@ -20,9 +27,13 @@ import org.springframework.web.client.RestTemplate;
  */
 @Service
 public class PaymentService {
+    final MediaType JSON = MediaType.get("application/json; charset=utf-8");
 
     @Autowired
     private WaitingPaymentRepository waitingPaymentRepository;
+
+    @Autowired
+    private Environment env;
 
     /**
      * Salva il link nel DB
@@ -70,20 +81,59 @@ public class PaymentService {
      * del pagamento
      * @param url url utente
      * @param paymentToken token del pagamento
+     * @throws IOException
      */
-    public void sendPaymentNotification(String url, String paymentToken) {
+    public void sendPaymentNotification(String url, String paymentToken) throws IOException {
         if(url == null) return;
 
-        RestTemplate restTemplate = new RestTemplate();
-        URI uri;
+        String token;
         try {
-            uri = new URI(url);
-            ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
-            //Check result.getStatusCodeValue()
-            System.out.println("Request "+url+" "+result.getStatusCode());
-        } catch (URISyntaxException e) {
-            // Bad Url
+            token = acmeskyAuth();
+        } catch (AuthRequestException | IOException e) {
+            System.out.println("Auth error:" + e.toString());
+            throw new IOException();
         }
-            
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                                    .url(url)
+                                    .addHeader("Content-Type", "application/json")
+                                    .addHeader("Authorization", "Bearer " + token)
+                                    .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            System.out.println("Request " + url + " " + response.code());
+        }
     }
+
+    public String acmeskyAuth() throws AuthRequestException, IOException{
+        AcmeskyAuthRequestDTO authRequest = new AcmeskyAuthRequestDTO();
+        authRequest.username = "bank";
+        authRequest.password = "bank";
+
+        String wsAddress = env.getProperty("server.acmesky.hostname");
+        String url = wsAddress+"/auth";
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        String jsonRequest = objectMapper.writeValueAsString(authRequest);
+        RequestBody body = RequestBody.create(jsonRequest, JSON);
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                                    .url(url)
+                                    .addHeader("Content-Type", "application/json")
+                                    .post(body)
+                                    .build();
+
+        String jsonResponse;
+        try (Response response = client.newCall(request).execute()) {
+            jsonResponse = response.body().string();
+            if(response.code() != 200)
+                throw new AuthRequestException();
+        }
+        AcmeskyAuthResponseDTO response = objectMapper.readValue(jsonResponse, AcmeskyAuthResponseDTO.class);
+        return response.token;	
+    }
+
+    public class AuthRequestException extends Exception {}
 }
