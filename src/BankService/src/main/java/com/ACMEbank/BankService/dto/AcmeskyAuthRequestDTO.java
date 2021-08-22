15ff02d6d15e7b7d68381ff5c43b852d1c4267b1
Vallasc <giacomovallorani.97@gmail.com>
2021-08-22 12:35:05 +0200

    Initial commit

---
+++ src/BankService/src/main/java/com/ACMEbank/BankService/dto/AcmeskyAuthRequestDTO.java
@@ -0,0 +1,8 @@
+package com.ACMEbank.BankService.dto;
+
+public class AcmeskyAuthRequestDTO {
+    public String username;
+    public String password;
+
+    public AcmeskyAuthRequestDTO(){}
+}
---
+++ src/BankService/src/main/java/com/ACMEbank/BankService/dto/AcmeskyAuthResponseDTO.java
@@ -0,0 +1,7 @@
+package com.ACMEbank.BankService.dto;
+
+public class AcmeskyAuthResponseDTO {
+    public String token;
+    
+    public AcmeskyAuthResponseDTO(){}
+}
