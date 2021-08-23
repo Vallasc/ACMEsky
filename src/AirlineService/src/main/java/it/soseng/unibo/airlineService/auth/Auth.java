ca80e54a98901e29ebe0ab5c1a91ff6b551303f9
andiub97 <43032657+andiub97@users.noreply.github.com>
2021-08-23 15:45:01 +0200

    updAuth

--- src/AirlineService/src/main/java/it/soseng/unibo/airlineService/auth/Auth.java
+++ src/AirlineService/src/main/java/it/soseng/unibo/airlineService/auth/Auth.java
@@ -3,41 +3,56 @@ package it.soseng.unibo.airlineService.auth;
 import java.io.IOException;
 import java.net.UnknownHostException;
 import java.util.Collections;
+import java.util.logging.Logger;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import it.soseng.unibo.airlineService.DTO.AuthRequest;
 import it.soseng.unibo.airlineService.DTO.UserRequest;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class Auth {
 
-    public String AuthRequest(String ACMEskyRoute, String username, String password)
-            throws JsonProcessingException, UnknownHostException {
+    private final static Logger LOGGER = Logger.getLogger(Auth.class.getName());
 
-        String url = ACMEskyRoute + "/auth";
+    final MediaType JSON = MediaType.get("application/json; charset=utf-8");
 
-        // // create headers
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
+    public String AuthRequest(String ACMEskyRoute, String username, String password) throws IOException {
+
+        String url = ACMEskyRoute + "auth";
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        OkHttpClient client = new OkHttpClient();
+
         AuthRequest req = new AuthRequest();
         req.setUsername(username);
         req.setPassword(password);
-        ObjectMapper mapper = new ObjectMapper();
 
-        String jsonCredentials = mapper.writeValueAsString(req);
-        // // build the request
-        HttpEntity<String> entity = new HttpEntity<String>(jsonCredentials, headers);
-        // // send POST request
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> jwt = restTemplate.postForEntity(url, entity, String.class);
-        return jwt.toString();
+        String requestBody = objectMapper.writeValueAsString(req);
+
+        RequestBody body = RequestBody.create(requestBody, JSON);
+        Request request = new Request.Builder().url(url).addHeader("Content-Type", "application/json").post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            if (response.code() != 200) {
+                LOGGER.severe(String.valueOf(response.code()));
+            } else {
+                JsonNode token = objectMapper.readTree(response.body().string());
+                return token.get("token").textValue();
+            }
+            return null;
+        }
 
     }
 }
